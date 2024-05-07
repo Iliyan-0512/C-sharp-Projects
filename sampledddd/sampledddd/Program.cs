816b095b4fecdc6c31aed9ea82cb94dd11866942
Iliyan <imanov05@abv.bg>
2024-05-07 21:47:54 +0300

    sample

---
+++ sampledddd/sampledddd/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("the best sample");
+            int[] bumber = { 1, 2, 3 };
+            for (int i = 0; i < bumber.Length; i++)
+            {
+                Console.WriteLine(bumber[i]+1);
+            }
+        }
+    }
+}
