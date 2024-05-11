e0b1d93ee1a2ff8ce97e869b2c4d79ba4dcee260
Iliyan <imanov05@abv.bg>
2024-05-11 10:28:25 +0300

    inheritance basic

---
+++ Inhearitance/amplecdsdv/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = Console.ReadLine().Split(" ").ToList;
+            
+        }
+    }
+}
