7a0974232ce54ae89f13fc5517c7d7100e713a32
Iliyan <imanov05@abv.bg>
2024-05-19 17:45:33 +0300

    task 4 from lab

---
+++ Inhearitance/4. Random List/CustomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomStringList : List<string>
+    {
+        private Random random;
+        public RandomStringList()
+        {
+            random=new Random();
+        }
+        public string GetRandomElement()
+        {
+            var index=random.Next(0,this.Count);
+            return this[index];
+        }
+        public void RemoveRandomElement()
+        {
+            var index=random.Next(0,Count);
+            RemoveAt(index);
+        }
+    }
+}
---
+++ Inhearitance/4. Random List/Program.cs
@@ -0,0 +1,12 @@
+﻿using CustomRandomList;
+
+var list=new RandomStringList();
+list.Add("1");
+list.Add("2");
+list.Add("3");
+list.Add("1");
+list.Add("1");
+for (int i = 0; i <10; i++)
+{
+    Console.WriteLine(list.GetRandomElement());
+}
