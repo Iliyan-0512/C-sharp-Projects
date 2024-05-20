69153dc28af074dbcaf415d4e30b071f523be78a
Iliyan <imanov05@abv.bg>
2024-05-20 19:34:17 +0300

    task person from inheritance exr

---
+++ Inhearitance/person/Child.cs
@@ -0,0 +1,27 @@
+﻿using Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Child:Person
+    {
+        public Child(int age,string name)
+            : base(age,name)
+        {
+           if(age == 0)
+            {
+                return;
+            }
+           else if(age >15)
+            {
+                return ;
+            }
+        }
+        
+
+    }
+}
---
+++ Inhearitance/person/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private int Age;
+        private string Name;
+        public Person(int age,string name) 
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format("Name: {0}, Age: {1}",this.Name,this.Age));
+            return sb.ToString() ;
+               
+
+        }
+
+
+    }
+}
