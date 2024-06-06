c04abd86d733bfa845e315748daead1fcebadaf4
Iliyan-0512 <imanov05@abv.bg>
2024-06-06 14:31:56 +0300

    task 4 from the lab

---
+++ Enscapulation/Encapsulation/4. First and Reserve Team/Person.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot be less than 3 symbols");
+                }
+                this.firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot be less than 3 symbols");
+                }
+                this.lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                //if (value < 650)
+                //{
+                //    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                //}
+                this.salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {String.Format("{0:0.00}", this.Salary)} leva";
+        }
+
+        public void Increasing(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+    }
+}
