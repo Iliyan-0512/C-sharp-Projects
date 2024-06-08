11af96228d5bf0bfcd498b130e11a591f0aedf94
Iliyan-0512 <imanov05@abv.bg>
2024-06-08 20:45:15 +0300

    task first from exr of ens

---
+++ Enscapulation/Encapsulation/1. Class Box Data/Box.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+        public double Length
+        {
+            get { return lenght; }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(lenght)} cannot be zero or negative.");
+                }
+                lenght = value;
+            }
+        }
+        public  double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(width)} cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(height)} cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public Box(double lenght,double width,double height)
+        {
+            this.Length = lenght;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double SurfaceArea()
+        {
+            
+           return  2*lenght * width + 2*lenght*height +2*width *height ;
+        }
+        public double LateralSurfaceArea()
+        {
+           
+            return 2 * lenght * height + 2 * width * height;
+        }
+        public  double Volume()
+        {
+            
+            return lenght * width * height;
+        }
+    }
+}
---
+++ Enscapulation/Encapsulation/1. Class Box Data/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Box
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                double lenght = double.Parse(Console.ReadLine());
+                double wight = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(lenght,wight,height);
+
+                Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+                Console.WriteLine($"Volume - {box.Volume():F2}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+        }
+    }
+}
