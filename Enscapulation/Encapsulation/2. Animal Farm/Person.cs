392cbc4c1b197616ef76379e804a48de66787e50
Iliyan-0512 <imanov05@abv.bg>
2024-06-09 22:53:46 +0300

    dont ready

---
+++ Enscapulation/Encapsulation/2. Animal Farm/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoping
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bags;
+         public string Name
+        {
+            get { return name; } 
+            set
+            {
+                if(name ==string.Empty)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+            }
+        }
+        public decimal Money
+        {
+            get { return money; }
+            set
+            {
+                if(value<0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+            }
+        }
+        public Person(string name, decimal money, List<Product> bags)
+        {
+            Name = name;
+            Money = money;
+            this.bags = bags;
+            
+        }
+    }
+}
---
+++ Enscapulation/Encapsulation/2. Animal Farm/product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoping
+{
+    public class Product
+    {
+        private string name;
+        private double cost;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name == string.Empty)
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+            }
+        }
+        public double Cost
+        {
+            get { return cost; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+            }
+        }
+    }
+}
