541282bfa88fd041dea80f00abd3437a7c1af078
Iliyan-0512 <imanov05@abv.bg>
2024-06-11 23:18:35 +0300

    dont ready

---
+++ Enscapulation/Encapsulation/4. Pizza Calories/Dough.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pizza
+{
+    public class Dough
+    {
+        private double white=1.5;
+        private double wholegrain=1;
+        private double crispy=0.9;
+        private double chewy=1.1;
+        private double homemade=1;
+        public double White
+        {
+            get { return white; } 
+            set
+            {
+
+            }
+        }
+        public double Wholegrain
+        {
+            get { return wholegrain; }
+            set
+            {
+
+            }
+        }
+        public double Crispy
+        {
+            get { return crispy; }
+            set
+            {
+
+            }
+        }
+        public double Chewy
+        {
+            get { return chewy; }
+            set
+            {
+
+            }
+        }
+        public double Homemade
+        {
+            get { return homemade; }
+            set
+            {
+
+            }
+        }
+
+
+
+
+
+    }
+}
