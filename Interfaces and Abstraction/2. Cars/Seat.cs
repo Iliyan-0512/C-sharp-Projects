3855783caa992a7fcc41759f49c95507cbd63309
Iliyan-0512 <imanov05@abv.bg>
2024-06-14 12:36:30 +0300

    ready

---
+++ Interfaces and Abstraction/2. Cars/Seat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Seat:ICar
+    {
+        public string Model { get; private set; }
+        public string Color { get; private set; }
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breeaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model}\n{Start()}\n{Stop()}";
+        }
+    }
+}
---
+++ Interfaces and Abstraction/2. Cars/Tesla.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla:ICar,IElectricCar
+
+    {
+       public string Model { get; private set; }
+        public string Color { get; private set; }
+        public int Battery { get; private set; }
+        public Tesla (string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breeaak!";
+        }
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Batteries\n{Start()}\n{Stop()}";
+        }
+
+    }
+}
