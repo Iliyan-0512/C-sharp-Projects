3815940cf71e19896e6235436258269dd3f2119d
Iliyan-0512 <imanov05@abv.bg>
2024-06-14 13:58:02 +0300

    task one from ex

---
+++ Interfaces and Abstraction/Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    public  class Citizen:IPerson
+    {
+        private string name;
+        private int age;
+        public Citizen(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Age
+        {
+            get { return age; }
+        }
+    }
+}
