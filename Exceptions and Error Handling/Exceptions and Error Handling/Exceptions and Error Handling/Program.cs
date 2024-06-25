0d4e073905c61dab76a7ea85f798321ec5786001
Iliyan-0512 <imanov05@abv.bg>
2024-06-25 17:54:59 +0300

    task 1 from handke ex

---
+++ Exceptions and Error Handling/Exceptions and Error Handling/Exceptions and Error Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    throw new FormatException();
+                }
+                double rezult = Math.Sqrt(number);
+                Console.WriteLine(rezult);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
