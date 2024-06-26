56d979ec75f425e4a07f0ec0e80abcf3d986ff7b
Iliyan-0512 <imanov05@abv.bg>
2024-06-26 10:52:20 +0300

    task 2

---
+++ Exceptions and Error Handling/Exceptions and Error Handling/2. Enter Numbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+
+
+            int[] numbers = new int[10];
+            int Start = 1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                numbers[i] = ReadNumber(Start, 100);
+                Start = numbers[i];
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static int ReadNumber(int start, int end)
+    {
+        while (true)
+        {
+          
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = int.Parse(input);
+
+                if (number <= start || number >= end)
+                {
+                    Console.WriteLine($"Your number is not in range {start} - {end}!");
+                    continue;
+                }
+
+
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number!");
+            }
+           
+        }
+    }
+}
