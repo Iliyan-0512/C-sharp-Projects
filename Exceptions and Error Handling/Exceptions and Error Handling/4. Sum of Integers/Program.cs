0a5a71d37c846ea2431ac8b7c88f76e19c4ebb13
Iliyan-0512 <imanov05@abv.bg>
2024-06-30 15:50:04 +0300

    task 4 from lab in handle ex

---
+++ Exceptions and Error Handling/Exceptions and Error Handling/4. Sum of Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string input = Console.ReadLine();
+            string[] arr = input.Split();
+
+
+            
+
+            for (int i = 0; i < arr.Length; i++)
+                try
+                {
+                    int number = int.Parse(arr[i]);
+                    sum += number;
+                    
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{arr[i]}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{arr[i]}' is out of range!");
+                }
+                finally
+                {
+
+                    Console.WriteLine($"Element '{arr[i]}' processed - current sum: {sum}");
+                }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
