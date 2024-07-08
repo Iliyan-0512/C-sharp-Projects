0b777203172b207f10f6eee2276911194cc15f42
Iliyan-0512 <imanov05@abv.bg>
2024-07-08 16:14:09 +0300

    task5 attribute

---
+++ Reflection and Attributes/Reflection and Attributes/5. Create Attribute/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthorProblem
+{
+    public class Author: Attribute
+    {
+        public string Name { get; }
+
+        public Author(string name)
+        {
+            Name = name;
+        }
+    }
+}
