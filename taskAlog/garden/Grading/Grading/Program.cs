c84d828d53b9c0b8e24d366dd5b642bcf3f2c56b
Iliyan-0512 <imanov05@abv.bg>
2024-09-25 14:56:26 +0300

    Турнир за Купата на Декана, 2009

---
+++ taskAlog/garden/Grading/Grading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int L = int.Parse(Console.ReadLine());
+            int R = int.Parse(Console.ReadLine());
+            int odd=0;
+            int even = 0; 
+            for (int i = L; i <= R; i++)
+            {
+                if (L%2==0 )
+                {
+                    even += i;
+                    L++;
+                }
+                else
+                {
+                    odd += i;
+                    L++;
+                }
+               
+            }
+            Console.WriteLine(Math.Abs(even-odd)%5+2);
+        }
+    }
+}
