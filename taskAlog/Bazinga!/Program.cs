1b00bbea95ada32703cc3884fb9179d6f011b916
Iliyan-0512 <imanov05@abv.bg>
2024-09-26 16:09:32 +0300

    Дизайн и Анализ на Алгоритми, 2011

---
+++ taskAlog/Bazinga!/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           string words=Console.ReadLine();
+            Dictionary<char, int> letterCount = new Dictionary<char, int>
+            {
+                { 'b',0 },
+                { 'a',0 },
+                { 'z',0 },
+                { 'i',0 },
+                { 'n',0 },
+                { 'g',0 },
+               
+            };
+            foreach (char item in words)
+            {
+                if (letterCount.ContainsKey(item))
+                {
+                    letterCount[item]++;
+
+                }
+            }
+            letterCount['a'] /= 2;
+            int maxBanners = letterCount.Values.Min();
+
+            
+            Console.WriteLine(maxBanners);
+        }
+    }
+}
