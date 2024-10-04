33ee4a3506ce7d4c05e17c7b1d9380798c356dae
Iliyan-0512 <imanov05@abv.bg>
2024-10-04 11:08:08 +0300

    IO

---
+++ lambda/taskAlog/IO/Program.cs
@@ -0,0 +1,29 @@
+﻿string  command =Console.ReadLine();
+
+if (command == "Array")
+{
+    
+    int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int[] reverse = new int[arr.Length]; 
+
+    
+    for (int i = arr.Length - 1; i >= 0; i--)
+    {
+        reverse[arr.Length - 1 - i] = arr[i]; 
+    }
+
+   
+    Console.WriteLine(string.Join(" ", reverse));
+}
+else if (command =="String")
+{
+    string original = Console.ReadLine();
+    string reversed = "";
+
+    for (int i = original.Length - 1; i >= 0; i--)
+    {
+        reversed += original[i];
+    }
+
+    Console.WriteLine(reversed);
+}
