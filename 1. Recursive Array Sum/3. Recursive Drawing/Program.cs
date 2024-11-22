27632db2b5408c158e9d25a98cb66927da8ba7df
Iliyan-0512 <imanov05@abv.bg>
2024-11-22 18:35:12 +0200

    Recursive Drawing

---
+++ 1. Recursive Array Sum/3. Recursive Drawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Drawing(n);
+        }
+        static int Drawing(int n)
+        {
+            if (n <=0)
+            {
+                return 0 ;
+            }
+            Console.WriteLine(new string('*',n));
+            int result =Drawing(n-1);
+            Console.WriteLine(new string('#',n));
+            return result + 1;
+        }
+       
+       
+    }
+}
