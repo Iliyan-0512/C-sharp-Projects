d160a03b28ccd11e0ec95dd42fa9abcbf63a34d7
Iliyan-0512 <imanov05@abv.bg>
2024-11-22 17:18:30 +0200

    task 2 from lab 2 softuni

---
+++ 1. Recursive Array Sum/2. Recursive Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        static long Factorial(int n)
+        {
+            if (n ==1) return 1;
+
+            return n*Factorial(  n - 1);
+        }
+    }
+}
