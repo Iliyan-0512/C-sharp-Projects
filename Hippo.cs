a1a8074673e734eb26258175fe659e271abebe11
Iliyan-0512 <62907829+Iliyan-0512@users.noreply.github.com>
2025-01-26 22:47:56 +0200

    Add files via upload

---
+++ Hippo.cs
@@ -0,0 +1,11 @@
+﻿class Hippo : Animal
+{
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Grunt.");
+    }
+    public void Swim()
+    {
+        Console.WriteLine("Splash! I'm going for a swim!");
+    }
+}
