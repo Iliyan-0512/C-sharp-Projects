8aa568d6a1c1cfbe94c989252f4b600431999fc8
Iliyan-0512 <imanov05@abv.bg>
2025-02-06 23:40:16 +0200

    Add two classes

---
+++ OOP-c#/To-do list/To-do list/TaskItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_do_list
+{
+    public class TaskItem
+    {
+       public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DueDate { get; set; }
+        public TaskItem() { }
+        public TaskItem(string title,string description,bool isComleted, DateTime dueDate) 
+        {
+            Title = title;
+            Description = description;
+            IsCompleted = isComleted;
+            DueDate = dueDate; 
+        }
+        public void MarkAsComplete()
+        {
+            IsCompleted = false;
+        }
+        public string UpdateTitle(string NewTitle)
+        {
+            Title= NewTitle;
+            return Title;
+        }
+        void UpdateDescribtion(string NewDеrscribtion)
+        {
+            Description = NewDеrscribtion;
+        }
+       
+    }
+}
---
+++ OOP-c#/To-do list/To-do list/ToDoList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace To_do_list
+{
+    public class ToDoList : TaskItem
+    {
+        public List<TaskItem> Tasks { get; set; } 
+
+        public ToDoList()
+            : base() 
+        {
+            Tasks = new List<TaskItem>();
+        }
+
+        public void AddTask(TaskItem task)
+        {
+            Tasks.Add(task); // Добавяне на TaskItem вместо грешния new Task()
+        }
+
+        public void RemoveTask(string title)
+        {
+            var taskToRemove = Tasks.FirstOrDefault(t => t.Title == title);
+            if (taskToRemove != null)
+            {
+                Tasks.Remove(taskToRemove);
+            }
+        }
+
+        public TaskItem GetTask(string title)
+        {
+            return Tasks.FirstOrDefault(t => t.Title == title);
+        }
+
+        public List<TaskItem> GetAllTasks()
+        {
+            return Tasks;
+        }
+
+        public List<TaskItem> GetPendingTasks()
+        {
+            return Tasks.Where(t => !t.IsCompleted).ToList();
+        }
+
+        public List<TaskItem> GetCompletedTasks()
+        {
+            return Tasks.Where(t => t.IsCompleted).ToList();
+        }
+    }
+}
