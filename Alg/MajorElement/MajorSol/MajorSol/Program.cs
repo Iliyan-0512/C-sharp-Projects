0d21edd29beea3a3295eef0d6a3ed33c18f20500
Iliyan-0512 <imanov05@abv.bg>
2025-02-13 23:30:47 +0200

    majorAlg

---
+++ Alg/MajorElement/MajorSol/MajorSol/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(MajorityElement(nums));
+
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            int candidate = -1;
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    candidate = nums[i];
+                    count = 1;
+                }
+                else
+                {
+                    if (nums[i] == candidate) count++;
+                    else count--;
+                }
+            }
+            count = 0;
+            for (int i=0; i < nums.Length; i++)
+            {
+                if (nums[i] == candidate) count++;
+            }
+            if(count>(nums.Length/2))
+            {
+                return candidate;
+            }
+            return -1;
+
+            
+        }
+    }
+}
