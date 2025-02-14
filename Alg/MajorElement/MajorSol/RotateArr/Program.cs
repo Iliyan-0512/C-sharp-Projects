06fb1d9cea95a259e918a979e5d98f9ed990aa9c
Iliyan-0512 <imanov05@abv.bg>
2025-02-14 23:00:54 +0200

    RotateAlgo

---
+++ Alg/MajorElement/MajorSol/RotateArr/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int k = int.Parse(Console.ReadLine());
+            RotateArr(nums, k);
+
+            Console.WriteLine(string.Join(",", nums));
+
+
+        }
+        public static void RotateArr(int[] nums, int k)
+        {
+            int n = nums.Length;
+            k = k % n;
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                res[(i + k) % n] = nums[i];
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = res[i];
+            }
+
+        }
+    }
+}
