4aa483e448b8127ab30fb4c05899a88ae4da8afa
Iliyan-0512 <imanov05@abv.bg>
2025-02-15 23:25:21 +0200

    JsonSer

---
+++ Text Readers and Writers/JsonSerialization/JsonSerialization/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var guys = new List<Guy>() {
+            new Guy() { Name = "Bob", Clothes = new Outfit() { Top = "t-shirt", Bottom = "jeans" },
+            Hair = new HairStyle() { Color = HairColor.Red, Length = 3.5f }},
+            new Guy() { Name = "Joe", Clothes = new Outfit() { Top = "polo", Bottom = "slacks" },
+            Hair = new HairStyle() { Color = HairColor.Gray, Length = 2.7f }},
+        };
+
+        var jsonString = JsonSerializer.Serialize(guys, new JsonSerializerOptions { WriteIndented = true });
+
+        Console.WriteLine(jsonString);
+
+        
+        
+        var copyOfGuys = JsonSerializer.Deserialize<List<Guy>>(jsonString);
+        foreach (var guy in copyOfGuys)
+            Console.WriteLine("I deserialized this guy: {0}", guy);
+    }
+}
+
+class Guy
+{
+    public string Name { get; set; }
+    public HairStyle Hair { get; set; }
+    public Outfit Clothes { get; set; }
+    public override string ToString() => $"{Name} with {Hair} wearing {Clothes}";
+}
+
+class Outfit
+{
+    public string Top { get; set; }
+    public string Bottom { get; set; }
+    public override string ToString() => $"{Top} and {Bottom}";
+}
+
+enum HairColor
+{
+    Auburn, Black, Blonde, Blue, Brown, Gray, Platinum, Purple, Red, White
+}
+
+class HairStyle
+{
+    public HairColor Color { get; set; }
+    public float Length { get; set; }
+    public override string ToString() => $"{Length:0.0} inch {Color} hair";
+}
---
+++ Text Readers and Writers/JsonSerialization/Unit8_Unit16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText("eureka.txt", "Eureka!");
+            byte[] eurekaBytes = File.ReadAllBytes("eureka.txt");
+            foreach (byte b in eurekaBytes)
+                Console.Write("{0} ", b);
+            Console.WriteLine(Encoding.UTF8.GetString(eurekaBytes));
+            Console.WriteLine(Path.GetFullPath("eureka.txt"));
+            Console.WriteLine(JsonSerializer.Serialize("ש"));
+
+        }
+    }
+}
