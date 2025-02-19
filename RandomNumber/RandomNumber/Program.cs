1de103bd0fefb1d40b9128ef107bb522853133eb
Iliyan-0512 <imanov05@abv.bg>
2025-02-19 23:10:31 +0200

    gameRandomNumber

---
+++ RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static Random random = new Random();
+
+    private static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Which Number is");
+        Console.WriteLine("Enter Nuber");
+        int randomNumber = random.Next(1,21);
+
+        Console.WriteLine (randomNumber.ToString());
+
+
+        int myNumber = int.Parse(Console.ReadLine());
+        int trySample = 5;
+        for (int i = 1; i < trySample; i++)
+        {
+            if (myNumber == randomNumber)
+            {
+                Console.WriteLine("Nice");
+                return;
+            }
+            else
+            {
+                if (myNumber > randomNumber)
+                {
+                    Console.WriteLine("NO,number is less");
+                }
+                else
+                {
+                    Console.WriteLine("NO,number is more bigger");
+                }
+                myNumber =int.Parse( Console.ReadLine());
+            }
+        }
+        Console.WriteLine("You lose");
+    }
+}
