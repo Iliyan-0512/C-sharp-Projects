b12b061bcb2a1ce3c17cc7ba75d08608f6944db8
Iliyan-0512 <imanov05@abv.bg>
2025-03-19 22:30:15 +0200

    task uzu

---
+++ PitagorNumber/Bankomat/Program.cs
@@ -0,0 +1,26 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int SumForSpend=int.Parse(Console.ReadLine());
+        Dictionary<int,int> nominal=new Dictionary<int,int>();
+       
+        nominal.Add(100, 10);
+        nominal.Add(50, 34);
+        nominal.Add(20, 14);
+        nominal.Add(10, 44);
+       
+    }
+    public void Menu(int choose,double totalSumWIt)
+    {
+        if (choose==1)
+        {
+            Withdraw(totalSumWIt);
+        }
+    }
+    public double Withdraw(double totalSumWIt)
+    {
+        return  
+    }
+}
---
+++ PitagorNumber/PitagorNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //a*a+b*b=c*c
+        for (int i = 1; i < 100; i++)
+        {
+            for (int j = 1; j < 100; j++)
+            {
+
+                for (int k = 1; k < 100; k++)
+                {
+                    if (i * i + j * j == k * k)
+                    {
+                        Console.WriteLine($"Pytogore number:{i} {j} {k}");
+                    }
+                }
+            }
+        }
+        var pitagorean = from a in Enumerable.Range(0, 100)
+                         from b in Enumerable.Range(0, 100)
+                         from c in Enumerable.Range(0, 100)
+                         where a * a + b * b == c * c
+                         select (a, b, c);
+        pitagorean.ToList().ForEach(x => Console.WriteLine($"{x.a},{x.b}, {x.c}"));
+                         
+
+    }
+}
