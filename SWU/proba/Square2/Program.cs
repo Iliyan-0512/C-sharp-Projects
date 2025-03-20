fb4a2fbf56e570e84891327405e117453e6afa03
Iliyan-0512 <imanov05@abv.bg>
2025-03-20 23:39:00 +0200

    oop encap

---
+++ SWU/proba/Square2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter length of side Squart");
+            //int n = int.Parse(Console.ReadLine());
+            //int s = n * n;
+            //Console.WriteLine($"S={s}");
+            //Console.WriteLine(1+1+"4"+2+1);
+            Console.Write("enter number: ");
+            string input = Console.ReadLine();
+            int length = input.Length;
+
+            string speed = length
+              switch
+            {
+
+                <= 10 => "slow",
+                <= 50 => "averege",
+                <= 150 => "fast",
+                _ => "very fast"
+            };
+
+            Console.WriteLine($"speed: {speed}");
+        }
+    }
+}
---
+++ SWU/proba/proba/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine($"He is {FirstName} {LastName}");
+        }
+    }
+}
