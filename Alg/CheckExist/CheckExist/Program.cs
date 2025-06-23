ef21c8c18f140b83e9ebddc9d851b7c8306dce8d
Iliyan-0512 <iliyan.Manov@proton.me>
2025-06-23 22:38:25 +0300

    remove exs

---
+++ Alg/CheckExist/CheckExist/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = [-20, 8, -6, -14, 0, -19, 14, 4];
+       Solution solution = new Solution();
+        
+        Console.WriteLine(solution.CheckIfExist(arr));
+    }
+}
