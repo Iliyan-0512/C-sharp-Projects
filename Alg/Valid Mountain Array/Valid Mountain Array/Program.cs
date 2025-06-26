14c8e2e26affb6eee98a026e4504587840c1129d
Iliyan-0512 <iliyan.Manov@proton.me>
2025-06-26 23:52:05 +0300

    add new task

---
+++ Alg/Valid Mountain Array/Valid Mountain Array/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Solution solution= new Solution();
+        int[] arr = [3, 5, 5];
+        Console.WriteLine(solution.ValidMountainArray(arr));
+    }
+}
