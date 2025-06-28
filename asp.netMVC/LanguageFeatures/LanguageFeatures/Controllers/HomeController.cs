9325a81af171787558486c47542ff58561222871
Iliyan-0512 <iliyan.Manov@proton.me>
2025-06-28 23:21:22 +0300

    init asp.netMVC project

---
+++ asp.netMVC/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+using LanguageFeatures.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> results = new List<string>();
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add(string.Format("Name: {0}, Price: {1}", name, price));
+                
+                string.Format("Name: {0}, Price: {1}, Related: {2}",name, price, relatedName);
+            }
+            return View(results);
+        }
+    }
+}
