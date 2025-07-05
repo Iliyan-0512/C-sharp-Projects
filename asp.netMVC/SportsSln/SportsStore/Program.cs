5b52fd3203355231b0e7cdefc45610c855d00cba
Iliyan-0512 <iliyan.Manov@proton.me>
2025-07-05 22:37:22 +0300

    Add db with ef

---
+++ asp.netMVC/SportsSln/SportsStore/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlite(
+    builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+var app = builder.Build();
+
+
+//app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+
+app.Run();
