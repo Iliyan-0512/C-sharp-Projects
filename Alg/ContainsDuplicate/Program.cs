835518e88fc78ecbeaaff13a21b33e616f910e95
Iliyan-0512 <iliyan.Manov@proton.me>
2025-08-02 22:00:09 +0300

    good solution for memoru but weak for effic

---
+++ Alg/ContainsDuplicate/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = [1, 2, 3, 1];
+        Solution solution = new Solution();
+        solution.ContainsDuplicate(array);
+        Console.WriteLine(solution);
+    }
+}
---
+++ Alg/ContainsDuplicate/Solution.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        bool number = false;
+        Array.Sort(nums);
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                number = true;
+                return number;
+            }
+        }
+        return number;
+    }
+}
